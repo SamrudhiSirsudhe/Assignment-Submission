69bdf04714d082bdde32ac1468c060988c2c4e62
Samrudhi Sirsudhe <103930511+SamrudhiSirsudhe@users.noreply.github.com>
2022-06-12 13:42:13 +0530

    Day 20 Assignment Submitted

---
+++ Day 20 Assignment/Untitled-1.cpp
@@ -0,0 +1,70 @@
+// implemetation of stack by there own
+#include<iostream>
+using namespace std;
+#define SIZE 5
+int A[SIZE];
+int top=-1;
+
+bool isempty()
+{
+	if(top==-1)
+		return true;
+	else
+		return false;
+		
+}
+
+void push(int value)
+{
+	if(top==SIZE-1)
+	{
+		cout<<"Stack is full\n";
+	}
+	else
+	{
+		top++;
+		A[top]=value;
+	}
+}
+
+void pop()
+{
+	if(isempty())
+		cout<<"Stack is empty!\n";
+	else top--;
+}
+
+void displayStack()
+{
+	if(isempty())
+	{
+		cout<<"Stack is empty!\n";
+	}
+	else{
+		for(int i=0 ; i<=top; i++)
+		cout<<A[i]<<" ";
+	}
+}
+
+void show_top()
+{
+	if(isempty())
+		cout<<"Stack is empty!\n";
+	else
+		cout<<"Element is top is: "<<A[top]<<"\n";
+}
+
+int main()
+{
+	push(2);
+	push(3);
+	push(4);
+	
+	displayStack();
+	
+	pop();
+	
+	show_top();
+	
+	return 0;
+}
